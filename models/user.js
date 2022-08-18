061345b02726cd3a918a787b85bbd918273a3dd4
Андрей <abobus21111@gmail.ru>
2022-08-18 15:44:32 +0300

    first

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchecma = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('card', userSchecma);
