61fdb0d491cdc7d04cfce67801f8ba9b2e06be6d
Андрей <abobus21111@gmail.ru>
2022-08-21 15:55:50 +0300

    three

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
+});
+
+app.use('/', userRouter);
+app.use('/', cardRouter);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
