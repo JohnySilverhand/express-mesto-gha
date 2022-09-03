7ac1db780699b8ab04ea0fcc2092a69661d6fc5e
Андрей <abobus21111@gmail.ru>
2022-09-03 21:02:22 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -124,7 +124,7 @@ module.exports.updateAvatar = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  return User.findUserByCrendentails(email, password)
+  return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'secret-key', { expiresIn: '7d' });
       return res.send({ token });
